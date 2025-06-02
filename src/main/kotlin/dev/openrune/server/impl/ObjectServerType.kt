15f1543c1510e4465acda97a9e2faf071f72ae21
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-06-02 12:28:10 -0400

    add: extension fns for door pair

--- src/main/kotlin/dev/openrune/server/impl/ObjectServerType.kt
+++ src/main/kotlin/dev/openrune/server/impl/ObjectServerType.kt
@@ -1,9 +1,10 @@
 package dev.openrune.server.impl
 
+import dev.openrune.cache.CacheManager.getObjects
 import dev.openrune.definition.Definition
 import dev.openrune.definition.type.ObjectType
 import dev.openrune.wiki.dumpers.impl.InfoBoxObject
-
+import kotlin.math.abs
 
 data class ObjectServerType(
     override var id: Int = -1,
@@ -25,6 +26,7 @@ data class ObjectServerType(
     var supportsItems: Int = -1,
     var isRotated: Boolean = false,
     var impenetrable: Boolean = true,
+    var replacementId: Int = -1,
     var varbit: Int = -1,
     var varp: Int = -1,
     var transforms: MutableList<Int>? = null,
@@ -52,6 +54,7 @@ data class ObjectServerType(
                 supportsItems = cache.supportsItems
                 isRotated = cache.isRotated
                 impenetrable = cache.impenetrable
+                replacementId = cache.oppositeDoorId()
                 varbit = cache.varbitId
                 varp = cache.varp
                 transforms = cache.transforms
@@ -61,5 +64,47 @@ data class ObjectServerType(
         }
     }
 
+    public fun ObjectType.hasOption(vararg searchOptions: String): Boolean {
+        return searchOptions.any { option ->
+            actions.any { it.equals(option, ignoreCase = true) }
+        }}
+
+    public fun ObjectType.getOption(vararg searchOptions: String): Int {
+        searchOptions.forEach {
+            actions.forEachIndexed { index, option ->
+                if (it.equals(option, ignoreCase = true)) return index + 1
+            }
+        }
+        return -1
+    }
+
+    public fun ObjectType.oppositeDoorId(values: Map<Int, ObjectType?> = getObjects()): Int {
+        if (getOption("open", "close") == -1) return -1
+
+        val ids = values.values
+            .filter { def ->
+                def != null &&
+                        def.id != id &&
+                        def.name == name &&
+                        def.modelSizeZ == modelSizeZ &&
+                        def.objectModels == objectModels &&
+                        def.objectTypes == objectTypes &&
+                        def.modifiedColours == modifiedColours &&
+                        def.isRotated == isRotated &&
+                        def.actions != actions &&
+                        def.actions.indices.all { i ->
+                            val s1 = def.actions[i]
+                            val s2 = actions[i]
+                            (s1 == s2) || (
+                                    ("open".equals(s1, ignoreCase = true) && "close".equals(s2, ignoreCase = true)) ||
+                                            ("close".equals(s1, ignoreCase = true) && "open".equals(s2, ignoreCase = true))
+                                    )
+                        }
+            }
+            .map { it!!.id }
+            .sortedBy { abs(it - id) }
+
+        return ids.firstOrNull() ?: -1
+    }
 }
 
