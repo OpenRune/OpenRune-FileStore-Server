c8ff059d1c87ef57235f81092cb47104b397caad
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-06-13 02:55:48 +0100

    fonts

---
+++ fonts/README.md
@@ -0,0 +1,225 @@
+# Old School RuneScape Fonts
+
+[![downloads](https://img.shields.io/github/downloads/runestar/fonts/total.svg)](https://github.com/runestar/fonts/releases/latest)
+[![license](https://img.shields.io/github/license/runestar/fonts)](LICENSE)
+[![chat](https://img.shields.io/discord/384870460640329728.svg?logo=discord)](https://discord.gg/G2kxrnU)
+
+![Preview](Preview.png)
+
+![Preview2](Preview2.png)
+
+[**Download**](https://github.com/runestar/fonts/releases/latest) in OpenType or TrueType
+
+Every font with all characters exactly as appears in-game
+
+| Name | Preview | Size | ID | Client Name | [Cache Name](https://github.com/runestar/cache-names) |
+| --- | --- | --- | --- | --- | --- |
+| Plain 11 | [link](previews/RuneScape/RuneScape-Plain-11.png) | 16px, 12pt | 494 | PLAIN11 | p11_full |
+| Plain 12 | [link](previews/RuneScape/RuneScape-Plain-12.png) | 16px, 12pt | 495 | PLAIN12 | p12_full |
+| Bold 12 | [link](previews/RuneScape/RuneScape-Bold-12.png) | 16px, 12pt | 496 | BOLD12 | b12_full |
+| Quill 8 | [link](previews/RuneScape/RuneScape-Quill-8.png) | 16px, 12pt | 497 | n/a | q8_full |
+| Quill | [link](previews/RuneScape/RuneScape-Quill.png) | 32px, 24pt | 645 | n/a | quill_oblique_large |
+| Quill Caps | [link](previews/RuneScape/RuneScape-Quill-Caps.png) | 64px, 48pt | 646 | n/a | quill_caps_large |
+| Fairy | [link](previews/RuneScape/RuneScape-Fairy.png) | 32px, 24pt | 647 | n/a | lunar_alphabet |
+| Fairy Large | [link](previews/RuneScape/RuneScape-Fairy-Large.png) | 64px, 48pt | 648 | n/a | lunar_alphabet_lrg |
+| Barbarian Assault | [link](previews/RuneScape/RuneScape-Barbarian-Assault.png) | 32px, 24pt | 764 | n/a | barbassault_font |
+| Surok | [link](previews/RuneScape/RuneScape-Surok.png) | 16px, 12pt | 819 | n/a | surok_font |
+
+| Character | [Byte](https://en.wikipedia.org/wiki/Windows-1252)/Code Point | Name |
+| --- | --- | --- |
+|   | 20 | SPACE |
+| ! | 21 | EXCLAMATION MARK |
+| " | 22 | QUOTATION MARK |
+| # | 23 | NUMBER SIGN |
+| $ | 24 | DOLLAR SIGN |
+| % | 25 | PERCENT SIGN |
+| & | 26 | AMPERSAND |
+| ' | 27 | APOSTROPHE |
+| ( | 28 | LEFT PARENTHESIS |
+| ) | 29 | RIGHT PARENTHESIS |
+| * | 2a | ASTERISK |
+| + | 2b | PLUS SIGN |
+| , | 2c | COMMA |
+| - | 2d | HYPHEN-MINUS |
+| . | 2e | FULL STOP |
+| / | 2f | SOLIDUS |
+| 0 | 30 | DIGIT ZERO |
+| 1 | 31 | DIGIT ONE |
+| 2 | 32 | DIGIT TWO |
+| 3 | 33 | DIGIT THREE |
+| 4 | 34 | DIGIT FOUR |
+| 5 | 35 | DIGIT FIVE |
+| 6 | 36 | DIGIT SIX |
+| 7 | 37 | DIGIT SEVEN |
+| 8 | 38 | DIGIT EIGHT |
+| 9 | 39 | DIGIT NINE |
+| : | 3a | COLON |
+| ; | 3b | SEMICOLON |
+| < | 3c | LESS-THAN SIGN |
+| = | 3d | EQUALS SIGN |
+| > | 3e | GREATER-THAN SIGN |
+| ? | 3f | QUESTION MARK |
+| @ | 40 | COMMERCIAL AT |
+| A | 41 | LATIN CAPITAL LETTER A |
+| B | 42 | LATIN CAPITAL LETTER B |
+| C | 43 | LATIN CAPITAL LETTER C |
+| D | 44 | LATIN CAPITAL LETTER D |
+| E | 45 | LATIN CAPITAL LETTER E |
+| F | 46 | LATIN CAPITAL LETTER F |
+| G | 47 | LATIN CAPITAL LETTER G |
+| H | 48 | LATIN CAPITAL LETTER H |
+| I | 49 | LATIN CAPITAL LETTER I |
+| J | 4a | LATIN CAPITAL LETTER J |
+| K | 4b | LATIN CAPITAL LETTER K |
+| L | 4c | LATIN CAPITAL LETTER L |
+| M | 4d | LATIN CAPITAL LETTER M |
+| N | 4e | LATIN CAPITAL LETTER N |
+| O | 4f | LATIN CAPITAL LETTER O |
+| P | 50 | LATIN CAPITAL LETTER P |
+| Q | 51 | LATIN CAPITAL LETTER Q |
+| R | 52 | LATIN CAPITAL LETTER R |
+| S | 53 | LATIN CAPITAL LETTER S |
+| T | 54 | LATIN CAPITAL LETTER T |
+| U | 55 | LATIN CAPITAL LETTER U |
+| V | 56 | LATIN CAPITAL LETTER V |
+| W | 57 | LATIN CAPITAL LETTER W |
+| X | 58 | LATIN CAPITAL LETTER X |
+| Y | 59 | LATIN CAPITAL LETTER Y |
+| Z | 5a | LATIN CAPITAL LETTER Z |
+| [ | 5b | LEFT SQUARE BRACKET |
+| \ | 5c | REVERSE SOLIDUS |
+| ] | 5d | RIGHT SQUARE BRACKET |
+| ^ | 5e | CIRCUMFLEX ACCENT |
+| _ | 5f | LOW LINE |
+| ` | 60 | GRAVE ACCENT |
+| a | 61 | LATIN SMALL LETTER A |
+| b | 62 | LATIN SMALL LETTER B |
+| c | 63 | LATIN SMALL LETTER C |
+| d | 64 | LATIN SMALL LETTER D |
+| e | 65 | LATIN SMALL LETTER E |
+| f | 66 | LATIN SMALL LETTER F |
+| g | 67 | LATIN SMALL LETTER G |
+| h | 68 | LATIN SMALL LETTER H |
+| i | 69 | LATIN SMALL LETTER I |
+| j | 6a | LATIN SMALL LETTER J |
+| k | 6b | LATIN SMALL LETTER K |
+| l | 6c | LATIN SMALL LETTER L |
+| m | 6d | LATIN SMALL LETTER M |
+| n | 6e | LATIN SMALL LETTER N |
+| o | 6f | LATIN SMALL LETTER O |
+| p | 70 | LATIN SMALL LETTER P |
+| q | 71 | LATIN SMALL LETTER Q |
+| r | 72 | LATIN SMALL LETTER R |
+| s | 73 | LATIN SMALL LETTER S |
+| t | 74 | LATIN SMALL LETTER T |
+| u | 75 | LATIN SMALL LETTER U |
+| v | 76 | LATIN SMALL LETTER V |
+| w | 77 | LATIN SMALL LETTER W |
+| x | 78 | LATIN SMALL LETTER X |
+| y | 79 | LATIN SMALL LETTER Y |
+| z | 7a | LATIN SMALL LETTER Z |
+| { | 7b | LEFT CURLY BRACKET |
+| \| | 7c | VERTICAL LINE |
+| } | 7d | RIGHT CURLY BRACKET |
+| ~ | 7e | TILDE |
+| € | 80/20ac | EURO SIGN |
+| Œ | 8c/152 | LATIN CAPITAL LIGATURE OE |
+| — | 97/2014 | EM DASH |
+| œ | 9c/153 | LATIN SMALL LIGATURE OE |
+| Ÿ | 9f/178 | LATIN CAPITAL LETTER Y WITH DIAERESIS |
+|   | a0 | NO-BREAK SPACE |
+| ¡ | a1 | INVERTED EXCLAMATION MARK |
+| ¢ | a2 | CENT SIGN |
+| £ | a3 | POUND SIGN |
+| ¤ | a4 | CURRENCY SIGN |
+| ¥ | a5 | YEN SIGN |
+| ¦ | a6 | BROKEN BAR |
+| § | a7 | SECTION SIGN |
+| ¨ | a8 | DIAERESIS |
+| © | a9 | COPYRIGHT SIGN |
+| ª | aa | FEMININE ORDINAL INDICATOR |
+| « | ab | LEFT-POINTING DOUBLE ANGLE QUOTATION MARK |
+| ¬ | ac | NOT SIGN |
+| ­ | ad | SOFT HYPHEN |
+| ® | ae | REGISTERED SIGN |
+| ¯ | af | MACRON |
+| ° | b0 | DEGREE SIGN |
+| ± | b1 | PLUS-MINUS SIGN |
+| ² | b2 | SUPERSCRIPT TWO |
+| ³ | b3 | SUPERSCRIPT THREE |
+| ´ | b4 | ACUTE ACCENT |
+| µ | b5 | MICRO SIGN |
+| ¶ | b6 | PILCROW SIGN |
+| · | b7 | MIDDLE DOT |
+| ¸ | b8 | CEDILLA |
+| ¹ | b9 | SUPERSCRIPT ONE |
+| º | ba | MASCULINE ORDINAL INDICATOR |
+| » | bb | RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK |
+| ¼ | bc | VULGAR FRACTION ONE QUARTER |
+| ½ | bd | VULGAR FRACTION ONE HALF |
+| ¾ | be | VULGAR FRACTION THREE QUARTERS |
+| ¿ | bf | INVERTED QUESTION MARK |
+| À | c0 | LATIN CAPITAL LETTER A WITH GRAVE |
+| Á | c1 | LATIN CAPITAL LETTER A WITH ACUTE |
+| Â | c2 | LATIN CAPITAL LETTER A WITH CIRCUMFLEX |
+| Ã | c3 | LATIN CAPITAL LETTER A WITH TILDE |
+| Ä | c4 | LATIN CAPITAL LETTER A WITH DIAERESIS |
+| Å | c5 | LATIN CAPITAL LETTER A WITH RING ABOVE |
+| Æ | c6 | LATIN CAPITAL LETTER AE |
+| Ç | c7 | LATIN CAPITAL LETTER C WITH CEDILLA |
+| È | c8 | LATIN CAPITAL LETTER E WITH GRAVE |
+| É | c9 | LATIN CAPITAL LETTER E WITH ACUTE |
+| Ê | ca | LATIN CAPITAL LETTER E WITH CIRCUMFLEX |
+| Ë | cb | LATIN CAPITAL LETTER E WITH DIAERESIS |
+| Ì | cc | LATIN CAPITAL LETTER I WITH GRAVE |
+| Í | cd | LATIN CAPITAL LETTER I WITH ACUTE |
+| Î | ce | LATIN CAPITAL LETTER I WITH CIRCUMFLEX |
+| Ï | cf | LATIN CAPITAL LETTER I WITH DIAERESIS |
+| Ð | d0 | LATIN CAPITAL LETTER ETH |
+| Ñ | d1 | LATIN CAPITAL LETTER N WITH TILDE |
+| Ò | d2 | LATIN CAPITAL LETTER O WITH GRAVE |
+| Ó | d3 | LATIN CAPITAL LETTER O WITH ACUTE |
+| Ô | d4 | LATIN CAPITAL LETTER O WITH CIRCUMFLEX |
+| Õ | d5 | LATIN CAPITAL LETTER O WITH TILDE |
+| Ö | d6 | LATIN CAPITAL LETTER O WITH DIAERESIS |
+| × | d7 | MULTIPLICATION SIGN |
+| Ø | d8 | LATIN CAPITAL LETTER O WITH STROKE |
+| Ù | d9 | LATIN CAPITAL LETTER U WITH GRAVE |
+| Ú | da | LATIN CAPITAL LETTER U WITH ACUTE |
+| Û | db | LATIN CAPITAL LETTER U WITH CIRCUMFLEX |
+| Ü | dc | LATIN CAPITAL LETTER U WITH DIAERESIS |
+| Ý | dd | LATIN CAPITAL LETTER Y WITH ACUTE |
+| Þ | de | LATIN CAPITAL LETTER THORN |
+| ß | df | LATIN SMALL LETTER SHARP S |
+| à | e0 | LATIN SMALL LETTER A WITH GRAVE |
+| á | e1 | LATIN SMALL LETTER A WITH ACUTE |
+| â | e2 | LATIN SMALL LETTER A WITH CIRCUMFLEX |
+| ã | e3 | LATIN SMALL LETTER A WITH TILDE |
+| ä | e4 | LATIN SMALL LETTER A WITH DIAERESIS |
+| å | e5 | LATIN SMALL LETTER A WITH RING ABOVE |
+| æ | e6 | LATIN SMALL LETTER AE |
+| ç | e7 | LATIN SMALL LETTER C WITH CEDILLA |
+| è | e8 | LATIN SMALL LETTER E WITH GRAVE |
+| é | e9 | LATIN SMALL LETTER E WITH ACUTE |
+| ê | ea | LATIN SMALL LETTER E WITH CIRCUMFLEX |
+| ë | eb | LATIN SMALL LETTER E WITH DIAERESIS |
+| ì | ec | LATIN SMALL LETTER I WITH GRAVE |
+| í | ed | LATIN SMALL LETTER I WITH ACUTE |
+| î | ee | LATIN SMALL LETTER I WITH CIRCUMFLEX |
+| ï | ef | LATIN SMALL LETTER I WITH DIAERESIS |
+| ð | f0 | LATIN SMALL LETTER ETH |
+| ñ | f1 | LATIN SMALL LETTER N WITH TILDE |
+| ò | f2 | LATIN SMALL LETTER O WITH GRAVE |
+| ó | f3 | LATIN SMALL LETTER O WITH ACUTE |
+| ô | f4 | LATIN SMALL LETTER O WITH CIRCUMFLEX |
+| õ | f5 | LATIN SMALL LETTER O WITH TILDE |
+| ö | f6 | LATIN SMALL LETTER O WITH DIAERESIS |
+| ÷ | f7 | DIVISION SIGN |
+| ø | f8 | LATIN SMALL LETTER O WITH STROKE |
+| ù | f9 | LATIN SMALL LETTER U WITH GRAVE |
+| ú | fa | LATIN SMALL LETTER U WITH ACUTE |
+| û | fb | LATIN SMALL LETTER U WITH CIRCUMFLEX |
+| ü | fc | LATIN SMALL LETTER U WITH DIAERESIS |
+| ý | fd | LATIN SMALL LETTER Y WITH ACUTE |
+| þ | fe | LATIN SMALL LETTER THORN |
+| ÿ | ff | LATIN SMALL LETTER Y WITH DIAERESIS |
---
+++ fonts/build.py
@@ -0,0 +1,264 @@
+from json import load
+from os import path
+import shutil
+import subprocess
+
+from fontTools.ttLib import *
+from fontTools.ttLib.tables._c_m_a_p import *
+from fontTools.ttLib.tables._m_a_x_p import *
+from fontTools.ttLib.tables._h_m_t_x import *
+from fontTools.ttLib.tables._p_o_s_t import *
+from fontTools.ttLib.tables._n_a_m_e import *
+from fontTools.ttLib.tables._h_e_a_d import *
+from fontTools.ttLib.tables.O_S_2f_2 import *
+from fontTools.ttLib.tables._h_h_e_a import *
+from fontTools.ttLib.tables._g_l_y_f import *
+from fontTools.ttLib.tables._l_o_c_a import *
+from fontTools.ttLib.tables._g_a_s_p import *
+
+logging.basicConfig(level=logging.WARNING)
+revision = '1.103'
+timestamp = timestampNow()
+
+
+def make_font(json, ttf, ttx, otf):
+    font_name = str(path.splitext(path.basename(ttf))[0])
+    font_name_human = font_name.replace('-', ' ')
+    game_name = font_name.split('-')[0]
+    with open(json) as f:
+        data = load(f)
+
+    for glyph in data['glyphs']:
+        glyph['name'] = Unicode[glyph['codePoint']]
+    data['glyphs'] = [g for g in data['glyphs'] if g['name'] != '????']
+    data['maxAdvance'] = max((g['advance'] for g in data['glyphs']))
+    data['maxDim'] = max(data['maxAdvance'], data['maxAscent'])
+
+    font = TTFont(recalcTimestamp=False)
+
+    font.glyphOrder = ['.notdef'] + [g['name'] for g in data['glyphs']]
+    if 'NO-BREAK SPACE' not in font.glyphOrder:
+        font.glyphOrder.append('NO-BREAK SPACE')
+
+    font['post'] = post = table__p_o_s_t()
+    post.formatType = 2.0
+    post.italicAngle = 0.0
+    post.underlinePosition = -1
+    post.underlineThickness = 1
+    post.isFixedPitch = 0
+    post.minMemType42 = post.maxMemType42 = post.minMemType1 = post.maxMemType1 = 0
+    post.glyphOrder = font.glyphOrder
+    post.extraNames = []
+    post.mapping = {}
+
+    font['name'] = name = table__n_a_m_e()
+    for platform in ((1, 0, 0), (3, 1, 0x409)):  # (Mac, Roman, English), (Windows, Unicode BMP, English US)
+        name.setName('Public Domain', 0, *platform)  # copyright
+        name.setName(font_name_human, 1, *platform)
+        name.setName('Regular', 2, *platform)
+        name.setName(font_name_human, 3, *platform)
+        name.setName(font_name_human, 4, *platform)
+        name.setName('Version ' + revision, 5, *platform)
+        name.setName(font_name, 6, *platform)
+        name.setName('Jagex Ltd.', 8, *platform)  # manufacturer
+        name.setName('Jagex Ltd.', 9, *platform)  # designer
+        name.setName('http://runestar.org', 11, *platform)  # vendor url
+        name.setName('Public Domain', 13, *platform)  # license
+        name.setName(game_name + ' is a registered trademark of Jagex Ltd.', 7, *platform)  # trademark notice
+        if game_name == 'RuneScape':
+            name.setName('From Old School RuneScape, a fantasy MMORPG', 10, *platform)  # description
+            name.setName('http://oldschool.runescape.com', 12, *platform)  # designer url
+        elif game_name == 'FunOrb':
+            name.setName('From FunOrb, a casual gaming site', 10, *platform)
+            name.setName('http://funorb.com', 12, *platform)
+
+    font['maxp'] = maxp = table__m_a_x_p()
+    maxp.tableVersion = 0x10000
+    maxp.numGlyphs = maxp.maxPoints = maxp.maxContours = 0  # auto
+    maxp.maxCompositePoints = 0
+    maxp.maxCompositeContours = 0
+    maxp.maxZones = 1
+    maxp.maxTwilightPoints = 0
+    maxp.maxStorage = 0
+    maxp.maxFunctionDefs = 0
+    maxp.maxInstructionDefs = 0
+    maxp.maxStackElements = 0
+    maxp.maxSizeOfInstructions = 0
+    maxp.maxComponentElements = 0
+    maxp.maxComponentDepth = 0
+
+    font['cmap'] = cmap = table__c_m_a_p()
+    cmap.tableVersion = 0
+    cmapWin = cmap_format_4(4)
+    cmapWin.platformID = 3  # Windows
+    cmapWin.platEncID = 1  # Unicode BMP
+    cmapWin.language = 0
+    cmapWin.cmap = {g['codePoint']: g['name'] for g in data['glyphs']}
+    cmapWin.cmap[160] = 'NO-BREAK SPACE'
+    cmap.tables = [cmapWin]
+
+    font['hmtx'] = hmtx = table__h_m_t_x()
+    hmtx.metrics = {g['name']: (g['advance'], g['leftBearing']) for g in data['glyphs']}
+    if 'NO-BREAK SPACE' not in hmtx.metrics:
+        hmtx.metrics['NO-BREAK SPACE'] = hmtx.metrics['SPACE']
+    if 'QUESTION MARK' in hmtx.metrics:
+        hmtx.metrics['.notdef'] = hmtx.metrics['QUESTION MARK']
+    else:
+        hmtx.metrics['.notdef'] = hmtx.metrics['SPACE']
+
+    font['head'] = head = table__h_e_a_d()
+    head.macStyle = 0
+    head.flags = 0  # auto
+    head.modified = head.created = timestamp
+    head.magicNumber = 0x5F0F3CF5
+    head.tableVersion = 1.0
+    head.fontRevision = float(revision)
+    head.checkSumAdjustment = 0
+    head.unitsPerEm = max(16, 2 ** (maxPowerOfTwo(data['maxDim'] - 1) + 1))
+    head.xMin = head.yMin = head.xMax = head.yMax = 0  # auto
+    head.lowestRecPPEM = 8
+    head.fontDirectionHint = 2  # normal roman
+    head.indexToLocFormat = 0
+    head.glyphDataFormat = 0
+
+    font['OS/2'] = os2 = table_O_S_2f_2()
+    os2.version = 4
+    os2.xAvgCharWidth = 0  # auto
+    os2.usWeightClass = 400  # normal
+    os2.usWidthClass = 5  # normal
+    os2.fsType = 0
+    os2.ySubscriptXSize = os2.ySubscriptYSize = os2.ySuperscriptXSize = os2.ySuperscriptYSize = head.unitsPerEm // 2
+    os2.ySubscriptXOffset = 0
+    os2.ySubscriptYOffset = 0
+    os2.ySuperscriptXOffset = 0
+    os2.ySuperscriptYOffset = 0
+    os2.yStrikeoutSize = 1
+    os2.yStrikeoutPosition = int(data['ascent'] * 0.70)
+    os2.sFamilyClass = 0
+    os2.panose = Panose()
+    os2.panose.bFamilyType = 0
+    os2.panose.bSerifStyle = 0
+    os2.panose.bWeight = 0
+    os2.panose.bProportion = 0
+    os2.panose.bContrast = 0
+    os2.panose.bStrokeVariation = 0
+    os2.panose.bArmStyle = 0
+    os2.panose.bLetterForm = 0
+    os2.panose.bMidline = 0
+    os2.panose.bXHeight = 0
+    os2.ulUnicodeRange1 = os2.ulUnicodeRange2 = os2.ulUnicodeRange3 = os2.ulUnicodeRange4 = 0  # auto
+    os2.achVendID = "h5wb"
+    os2.fsSelection = 0b11000000  # use typo metrics, regular
+    os2.usFirstCharIndex = os2.usLastCharIndex = 0  # auto
+    os2.sTypoAscender = data['maxAscent']
+    os2.sTypoDescender = data['maxDescent'] * -1
+    os2.sTypoLineGap = 0
+    os2.usWinAscent = data['maxAscent']
+    os2.usWinDescent = data['maxDescent']
+    os2.ulCodePageRange1 = 1  # cp-1252
+    os2.ulCodePageRange2 = 0
+    os2.sxHeight = os2.sCapHeight = 0  # auto
+    os2.usDefaultChar = 0
+    os2.usBreakChar = 32  # space
+    os2.usMaxContext = 0
+
+    font['hhea'] = hhea = table__h_h_e_a()
+    hhea.tableVersion = 0x00010000
+    hhea.ascent = data['maxAscent']
+    hhea.descent = data['maxDescent'] * -1
+    hhea.lineGap = 0
+    hhea.advanceWidthMax = 0  # auto
+    hhea.minLeftSideBearing = 0
+    hhea.minRightSideBearing = 0
+    hhea.xMaxExtent = 0  # auto
+    hhea.caretSlopeRise = 1
+    hhea.caretSlopeRun = 0
+    hhea.caretOffset = 0
+    hhea.reserved0 = hhea.reserved1 = hhea.reserved2 = hhea.reserved3 = 0
+    hhea.metricDataFormat = 0
+    hhea.numberOfHMetrics = 0  # auto
+
+    font['glyf'] = glyf = table__g_l_y_f()
+    glyf.glyphs = {}
+    for glyph in data['glyphs']:
+        g = Glyph()
+        g.program = None
+        g.xMin = g.yMin = g.xMax = g.yMax = 0  # auto
+        g.numberOfContours = 0
+        g.coordinates = []
+        g.flags = []
+        g.endPtsOfContours = []
+        x = glyph['leftBearing']
+        y = data['ascent'] - glyph['topBearing']
+        width = glyph['width']
+        for i, p in enumerate(glyph['pixels']):
+            x += 1
+            if i % width == 0:
+                x = 0
+                y -= 1
+            if p == 1:
+                g.numberOfContours += 1
+                coords = GlyphCoordinates()
+                coords.extend([(x, y), (x + 1, y), (x + 1, y + 1), (x, y + 1)])
+                g.coordinates.extend(coords)
+                g.flags.extend([True, True, True, True])
+                g.endPtsOfContours.append(len(g.coordinates) - 1)
+        glyf.glyphs[glyph['name']] = g
+    if 'NO-BREAK SPACE' not in glyf.glyphs:
+        glyf.glyphs['NO-BREAK SPACE'] = glyf.glyphs['SPACE']
+    if 'QUESTION MARK' in glyf.glyphs:
+        glyf.glyphs['.notdef'] = glyf.glyphs['QUESTION MARK']
+    else:
+        glyf.glyphs['.notdef'] = glyf.glyphs['SPACE']
+
+    font['loca'] = table__l_o_c_a()
+
+    font['gasp'] = gasp = table__g_a_s_p()
+    gasp.gaspRange = {0xFFFF: 0}
+
+    os2.recalcUnicodeRanges(font)
+
+    font.saveXML(ttx)
+    font2 = TTFont(recalcTimestamp=False)
+    font2.importXML(ttx)
+    font2.save(ttf)
+
+    subprocess.run(['fontforge', '-lang=py', '-script', 'fontforge_script.py', ttf, otf], shell=True)
+
+
+def make_preview(ttf, png):
+    font_name_human = str(path.splitext(path.basename(ttf))[0]).replace('-', ' ')
+    subprocess.run([
+        'magick', 'convert',
+        '-pointsize', '64',
+        '-font', ttf,
+        '-gravity', 'center',
+        f'label:{font_name_human}\\nABCDEFGHIJKLMNOPQRSTUVWXYZ\\nabcdefghijklmnopqrstuvwxyz\\n0123456789',
+        png
+    ], shell=True)
+
+
+for category in os.listdir('data'):
+    category_dir = path.join('data', category)
+    out_dir = 'out'
+    out_category_dir = path.join(out_dir, category)
+    ttf_dir = path.join(out_category_dir, 'ttf')
+    otf_dir = path.join(out_category_dir, 'otf')
+    preview_dir = 'previews'
+    preview_category_dir = path.join(preview_dir, category)
+    os.makedirs(ttf_dir, exist_ok=True)
+    os.makedirs(otf_dir, exist_ok=True)
+    os.makedirs(preview_category_dir, exist_ok=True)
+    for fileName in os.listdir(category_dir):
+        font_name = path.splitext(fileName)[0]
+        json = path.join(category_dir, fileName)
+        ttf = path.join(ttf_dir, font_name + '.ttf')
+        ttx = path.join(out_category_dir, font_name + '.ttx')
+        otf = path.join(otf_dir, font_name + '.otf')
+        print(font_name)
+        make_font(json, ttf, ttx, otf)
+        os.remove(ttx)
+        png = path.join(preview_category_dir, font_name + '.png')
+        make_preview(ttf, png)
+    zip = path.join(out_dir, category + "-Fonts")
+    shutil.make_archive(zip, 'zip', out_category_dir)
