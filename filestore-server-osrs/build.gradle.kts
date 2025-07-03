a9e0ca03fb796d488cf3e22c4d91afd62cf4f6f9
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2025-07-03 01:32:02 -0400

    feat: mainline definition opcode system

--- filestore-server-osrs/build.gradle.kts
+++ filestore-server-osrs/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-   implementation(project(":definition-opcodes"))
    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
    implementation("com.google.code.gson:gson:2.10.1")
 }
